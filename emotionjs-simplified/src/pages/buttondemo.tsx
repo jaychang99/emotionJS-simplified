a3f24591703f03d460f86e073901db779871ed40
jaychang99-macbook pro <86560973+jaychang99@users.noreply.github.com>
2022-07-06 04:44:32 +0900

    feature(frontend): align center items

--- emotionjs-simplified/src/pages/buttondemo.tsx
+++ emotionjs-simplified/src/pages/buttondemo.tsx
@@ -1,7 +1,11 @@
 import Button from "../components/common/Button";
 
 function ButtonDemoPage() {
-  return <Button>버튼입니다</Button>;
+  return (
+    <>
+      <Button>버튼입니다</Button>
+    </>
+  );
 }
 
 export default ButtonDemoPage;
