3323d7fcb6423e579ac8dee17ffa3b6e6b5ee4c7
jaychang99-macbook pro <86560973+jaychang99@users.noreply.github.com>
2022-07-06 16:43:51 +0900

    input.tsx removed, Container remains

--- emotionjs-simplified/src/pages/input.tsx
+++ emotionjs-simplified/src/pages/input.tsx
@@ -1,5 +1,4 @@
 import styled from "@emotion/styled";
-import Input from "../components/common/Input";
 
 const Container = styled.div`
   max-width: 400px;
@@ -12,12 +11,7 @@ const Container = styled.div`
 `;
 
 function InputPage() {
-  return (
-    <Container>
-      <Input label="이메일" placeholder="이메일을 입력하세요" />
-      <Input placeholder="이메일을 입력하세요" />
-    </Container>
-  );
+  return <p>this is input page</p>;
 }
 
 export default InputPage;
