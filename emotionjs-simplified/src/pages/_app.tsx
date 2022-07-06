0f316b658481f6a2db9e085f7d9c251d9297fc71
jaychang99-macbook pro <86560973+jaychang99@users.noreply.github.com>
2022-07-06 16:42:37 +0900

    blanked _app.tsx

--- emotionjs-simplified/src/pages/_app.tsx
+++ emotionjs-simplified/src/pages/_app.tsx
@@ -1,15 +1,7 @@
 import type { AppProps } from "next/app";
-import { theme } from "../styles/theme";
-import GlobalStyles from "../styles/GlobalStyles";
-import { ThemeProvider } from "@emotion/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
